fa5b57c17c9ae1531ed32fd350549156cd32447c
MichaS <micha@s.com>
2023-12-10 23:03:43 +0100

    trial & error for placing icon over image

--- src/app/playground/play3/play3.component.ts
+++ src/app/playground/play3/play3.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'nd-play3',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './play3.component.html',
   styleUrl: './play3.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
